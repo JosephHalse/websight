5316e4fc2e50bd59e07f16ef84db3aa161279738
jshalse <jshalse@users.noreply.github.com>
2018-03-07 17:33:40 +0000

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,6 +1,7 @@
 <?php
 
-echo  "<script>
+function listen(){
+return  "<script>
 var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
 recognition.lang = 'en-US';
 recognition.interimResults = false;
@@ -8,6 +9,8 @@ recognition.maxAlternatives = 5;
 recognition.start();
 recognition.onresult = function(event) {
 document.write(event.results[0][0].transcript);
-   };
+};
 </script>";
+}
 
+echo listen();
